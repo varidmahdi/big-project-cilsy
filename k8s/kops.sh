c64e98de1cae88c0324f4dbddb9f8071e9d9eb3f
varidmahdi <varidmahdi@gmail.com>
2021-08-20 13:48:34 +0700

    fix kops file

--- k8s/kops.sh
+++ k8s/kops.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-export KOPS_CLUSTER_NAME=k8scilsy.lokaljuara.id
+export KOPS_CLUSTER_NAME=lokaljuara.id
 export KOPS_STATE_STORE=s3://k8scilsy.lokaljuara.id
 
-$ kops create cluster --node-count=1 --node-size=t3a.small --master-size=t3a.small --zones=us-east-1a --name=${KOPS_CLUSTER_NAME} --ssh-public-key=d:/ssh_key/id_rsa.pub --cloud=aws --cloud-labels="Cost=cilsy"
+kops create cluster --node-count=1 --node-size=t3a.small --master-size=t3a.small --zones=us-east-1a --name=${KOPS_CLUSTER_NAME} --ssh-public-key=d:/ssh_key/id_rsa.pub --cloud=aws --cloud-labels="Cost=bigProjectCilsy"
 
 kops update cluster --name=${KOPS_CLUSTER_NAME} --yes --admin
+
+kops rolling-update cluster --cloudonly
+
+kops validate cluster --wait 10m
+
+kops delete cluster --name=${KOPS_CLUSTER_NAME} --yes
